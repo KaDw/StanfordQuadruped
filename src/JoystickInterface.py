32feb9d19e2c9a17ff5d867e13816ef4f6f8d4b5
KaDw <kardwor1@student.pg.gda.pl>
2020-09-12 00:46:19 +0200

    Replace PS4 with Xbox One S controller

--- src/JoystickInterface.py
+++ src/JoystickInterface.py
@@ -4,6 +4,8 @@ import time
 from src.State import BehaviorState, State
 from src.Command import Command
 from src.Utilities import deadband, clipped_first_order_filter
+import evdev
+from evdev import list_devices, ecodes
 
 
 class JoystickInterface:
@@ -17,67 +19,110 @@ class JoystickInterface:
         self.previous_activate_toggle = 0
 
         self.message_rate = 50
-        self.udp_handle = UDPComms.Subscriber(udp_port, timeout=0.3)
-        self.udp_publisher = UDPComms.Publisher(udp_publisher_port)
+        self.device = evdev.InputDevice(list_devices()[0])
+
+        print(self.device)
+
+        self.ls = [ 0.0, 0.0 ]
+        self.rs = [ 0.0, 0.0 ]
+        self.l1 = False
+        self.r1 = False
+        self.y = False
+        self.b = False
+        self.a = False
+        self.x = False
+        self.dpad_x = 0
+        self.dpad_y = 0
 
 
     def get_command(self, state, do_print=False):
         try:
-            msg = self.udp_handle.get()
-            command = Command()
+            for event in self.device.read():
+                if event.type == ecodes.EV_ABS:
+                    if event.code == ecodes.ABS_X:
+                        self.ls[0] = (event.value - 32767.0) / 32768.0
+                    elif event.code == ecodes.ABS_Y:
+                        self.ls[1] = (event.value - 32767.0) / 32768.0
+                    elif event.code == ecodes.ABS_RX:
+                        self.rs[0] = (event.value - 32767.0) / 32768.0
+                    elif event.code == ecodes.ABS_RY:
+                        self.rs[1] = (event.value - 32767.0) / 32768.0
+                    elif event.code == 16:
+                        self.dpad_x = event.value
+                    elif event.code == 17:
+                        self.dpad_y = event.value
+                elif event.type == ecodes.EV_KEY:
+                    if event.code == 308: # Code for Y
+                        self.y = event.value != 0
+                    if event.code == 305: # Code for B
+                        self.b = event.value != 0
+                    if event.code == 304: # Code for A
+                        self.a = event.value != 0
+                    if event.code == 307: # Code for X
+                        self.x = event.value != 0
+                    elif event.code == 310: # Code for L1
+                        self.l1 = event.value != 0
+                    elif event.code == 311: # Code for R1
+                        self.r1 = event.value != 0
             
-            ####### Handle discrete commands ########
-            # Check if requesting a state transition to trotting, or from trotting to resting
-            gait_toggle = msg["R1"]
-            command.trot_event = (gait_toggle == 1 and self.previous_gait_toggle == 0)
+                #print(event)
+        except:
+            pass
 
-            # Check if requesting a state transition to hopping, from trotting or resting
-            hop_toggle = msg["x"]
-            command.hop_event = (hop_toggle == 1 and self.previous_hop_toggle == 0)            
-            
-            activate_toggle = msg["L1"]
-            command.activate_event = (activate_toggle == 1 and self.previous_activate_toggle == 0)
+        assert(self.ls[0] >= -1.0 and self.ls[0] <= 1.0)
+        assert(self.ls[1] >= -1.0 and self.ls[1] <= 1.0)
+        assert(self.rs[0] >= -1.0 and self.rs[0] <= 1.0)
+        assert(self.rs[1] >= -1.0 and self.rs[1] <= 1.0)
 
-            # Update previous values for toggles and state
-            self.previous_gait_toggle = gait_toggle
-            self.previous_hop_toggle = hop_toggle
-            self.previous_activate_toggle = activate_toggle
+        command = Command()
+        
+        ####### Handle discrete commands ########
+        # Check if requesting a state transition to trotting, or from trotting to resting
+        gait_toggle = self.r1
+        command.trot_event = (gait_toggle == 1 and self.previous_gait_toggle == 0)
 
-            ####### Handle continuous commands ########
-            x_vel = msg["ly"] * self.config.max_x_velocity
-            y_vel = msg["lx"] * -self.config.max_y_velocity
-            command.horizontal_velocity = np.array([x_vel, y_vel])
-            command.yaw_rate = msg["rx"] * -self.config.max_yaw_rate
+        # Check if requesting a state transition to hopping, from trotting or resting
+        hop_toggle = self.x
+        command.hop_event = (hop_toggle == 1 and self.previous_hop_toggle == 0)            
+        
+        activate_toggle = self.l1
+        command.activate_event = (activate_toggle == 1 and self.previous_activate_toggle == 0)
 
-            message_rate = msg["message_rate"]
-            message_dt = 1.0 / message_rate
+        # Update previous values for toggles and state
+        self.previous_gait_toggle = gait_toggle
+        self.previous_hop_toggle = hop_toggle
+        self.previous_activate_toggle = activate_toggle
 
-            pitch = msg["ry"] * self.config.max_pitch
-            deadbanded_pitch = deadband(
-                pitch, self.config.pitch_deadband
-            )
-            pitch_rate = clipped_first_order_filter(
-                state.pitch,
-                deadbanded_pitch,
-                self.config.max_pitch_rate,
-                self.config.pitch_time_constant,
-            )
-            command.pitch = state.pitch + message_dt * pitch_rate
+        ####### Handle continuous commands ########
+        x_vel = self.ls[1] * -self.config.max_x_velocity
+        y_vel = self.ls[0] * -self.config.max_y_velocity
+        command.horizontal_velocity = np.array([x_vel, y_vel])
+        command.yaw_rate = self.rs[0] * -self.config.max_yaw_rate
 
-            height_movement = msg["dpady"]
-            command.height = state.height - message_dt * self.config.z_speed * height_movement
-            
-            roll_movement = - msg["dpadx"]
-            command.roll = state.roll + message_dt * self.config.roll_speed * roll_movement
+        message_rate = self.message_rate
+        message_dt = 1.0 / message_rate
 
-            return command
+        pitch = self.rs[1] * self.config.max_pitch
+        deadbanded_pitch = deadband(
+            pitch, self.config.pitch_deadband
+        )
+        pitch_rate = clipped_first_order_filter(
+            state.pitch,
+            deadbanded_pitch,
+            self.config.max_pitch_rate,
+            self.config.pitch_time_constant,
+        )
+        command.pitch = state.pitch + message_dt * pitch_rate
 
-        except UDPComms.timeout:
-            if do_print:
-                print("UDP Timed out")
-            return Command()
+        height_movement = self.dpad_y
+        command.height = state.height - message_dt * self.config.z_speed * height_movement
+        
+        roll_movement = -self.dpad_x
+        command.roll = state.roll + message_dt * self.config.roll_speed * roll_movement
 
+        return command
 
     def set_color(self, color):
-        joystick_msg = {"ps4_color": color}
-        self.udp_publisher.send(joystick_msg)
+        # joystick_msg = {"ps4_color": color}
+        # self.udp_publisher.send(joystick_msg)
+        pass
