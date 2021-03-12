045c51dce77d2836f9edbd1147cb44c53516bba6
Karol <kardwor1@student.pg.gda.pl>
2021-03-12 15:42:37 +0000

    Update calibration values

--- pupper/HardwareConfig.py
+++ pupper/HardwareConfig.py
@@ -6,7 +6,9 @@ import numpy as np
 
 MICROS_PER_RAD = 11.333 * 180.0 / np.pi  # Must be calibrated
 NEUTRAL_ANGLE_DEGREES = np.array(
-    [[ -0,   7,   2,   3], [ 17,  57, 46,  52], [-39, -35, -33, -64]]
+[[  1,   9,   8,   9,],
+ [ 48,  44,  53,  42,],
+ [-63, -54, -65, -59]]
 )
 
 PS4_COLOR = {"red": 0, "blue": 0, "green": 255}
